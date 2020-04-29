a479b5b73760509ed920dfda40e5522c5a800309
Roberto Castro <30873841+robertocastro7@users.noreply.github.com>
2020-04-28 21:40:04 -0600

    Update MainActivity.java

--- CinepolisApp2/app/src/main/java/com/tec/cinepolisapp2/MainActivity.java
+++ CinepolisApp2/app/src/main/java/com/tec/cinepolisapp2/MainActivity.java
@@ -5,7 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ExpandableListView;
 import android.widget.ImageButton;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ImageButton botonpp;
@@ -16,6 +21,10 @@ public class MainActivity extends AppCompatActivity {
     ImageButton botonPoster4;
     ImageButton botonPoster5;
     ImageButton botonPoster6;
+    private ExpandableListView listView;
+    private ExpandableListAdapter listAdapter;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listHash;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +39,10 @@ public class MainActivity extends AppCompatActivity {
         botonPoster4 = findViewById(R.id.poster4);
         botonPoster5 = findViewById(R.id.poster5);
         botonPoster6 = findViewById(R.id.poster6);
-
+        listView = (ExpandableListView) findViewById(R.id.textView2);
+        initData();
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listHash);
+        listView.setAdapter(listAdapter);
 
        boton4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,4 +110,20 @@ public class MainActivity extends AppCompatActivity {
         myIntent.putExtras(bundle);
         startActivity(myIntent);
     }
+
+    private void initData(){
+        listDataHeader = new ArrayList<>();
+        listHash = new HashMap<>();
+
+        listDataHeader.add("VIP Andares  3.7 km");
+
+        List<String> cines = new ArrayList<>();
+        cines.add("Real Center  1.2 km");
+        cines.add("Galerías Guadalajara  5.6 km");
+        cines.add("La Gran Plaza  6.0 km");
+        cines.add("Ciudadela Lifestyle Center  7.3 km");
+
+        listHash.put(listDataHeader.get(0), cines);
+    }
 }
+
